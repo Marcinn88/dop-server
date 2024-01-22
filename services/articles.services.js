538efef6b041067a0cc8e7312be22b5ccc9c6841
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2024-01-22 20:30:26 +0100

    Update articles.services.js

--- services/articles.services.js
+++ services/articles.services.js
@@ -4,28 +4,28 @@ const getAll = async (query) => {
     return Article.find(query)
 };
 
-const getOne = async (id, userId) => {
-    return Article.findById({ _id: id, owner: userId });
+const getOne = async (id) => {
+    return Article.findById({ _id: id });
 };
 
 const create = async (data) => {
     return Article.create(data);
 };
 
-const update = async (id, userId, data) => {
-    return Article.findOneAndUpdate({ _id: id, owner: userId }, data, {
+const update = async (id, data) => {
+    return Article.findOneAndUpdate({ _id: id }, data, {
         new: true,
     })
 };
 
-const updateStatus = async (id, userId, favorite) => {
-    return Article.findOneAndUpdate({ _id: id, owner: userId }, { favorite }, {
+const updateStatus = async (id, favorite) => {
+    return Article.findOneAndUpdate({ _id: id }, { favorite }, {
         new: true,
     })
 };
 
-const remove = async (id, userId) =>{
-    return Article.findOneAndDelete({_id: id, owner: userId})
+const remove = async (id) =>{
+    return Article.findOneAndDelete({_id: id })
 };
 
 module.exports = {
