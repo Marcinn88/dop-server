db2c25b087c898eb5bf41f2f3bd6c8b87562068c
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2024-01-23 21:12:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -39,13 +39,13 @@ const isAccessible = async path => {
   }
 };
 
-// const createFolderIsNotExist = async (folder) => {
-//   if (!(await isAccessible(folder))) {
-//     await fs.mkdir(folder, {
-//       recursive: true
-//     });
-//   }
-// }
+const createFolderIsNotExist = async (folder) => {
+  if (!(await isAccessible(folder))) {
+    await fs.mkdir(folder, {
+      recursive: true
+    });
+  }
+}
 
 app.use((req, res, next) =>{
   next(createError(404));
@@ -62,8 +62,8 @@ app.use((err, req, res, next) => {
 connection.then(()=>{
   console.log("Database connection succesfull.")
   app.listen(PORT, async () => {
-    // createFolderIsNotExist(config.UPLOADS_PATH);
-    // createFolderIsNotExist(config.IMAGES_PATH);
+    createFolderIsNotExist(config.UPLOADS_PATH);
+    createFolderIsNotExist(config.IMAGES_PATH);
     console.log(`Server running. Use our API on port: ${PORT}`)
   })
 
